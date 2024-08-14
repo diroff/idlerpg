a6c248f8d4a47ed86e48e765a0ab64f78806095b
Диэр <danil.aprelskiy@mail.ru>
2024-08-14 16:11:26 +0300

    feat: add melee/ranged weapon

---
+++ Assets/Scripts/Data/Weapon/MeleeWeaponStats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponStats", menuName = "RPG/Weapon Data/New Melee Weapon Data")]
+public class MeleeWeaponStats : WeaponStats
+{
+
+}
---
+++ Assets/Scripts/Data/Weapon/RangedWeaponStats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponStats", menuName = "RPG/Weapon Data/New Ranged Weapon Data")]
+public class RangedWeaponStats : WeaponStats
+{
+
+}
